21de5e0fb8d4623ffd3c09d4d7475192e3a35231
Nguyên <ngonguyenkhoinguyen@gmail.com>
2023-10-10 21:04:01 +0700

    Lab 2 completed ex1 to ex8

---
+++ Ex3/Core/Inc/software_timer.h
@@ -0,0 +1,10 @@
+#ifndef __SOFTWARE_TIMER__
+#define __SOFTWARE_TIMER__
+extern int timer1_flag;
+void setTimer1(int );
+void timer1Run();
+
+extern int timer2_flag;
+void setTimer2(int );
+void timer2Run();
+#endif
---
+++ Ex8/Core/Inc/software_timer.h
@@ -0,0 +1,8 @@
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer1_flag;
+void setTimer1(int duration);
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
